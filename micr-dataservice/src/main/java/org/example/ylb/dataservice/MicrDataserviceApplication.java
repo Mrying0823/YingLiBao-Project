2955601d41e13b7266de02c906795955696b433b
Mrying0823 <1553947217@qq.com>
2023-09-23 11:11:44 +0800

    设置 MyBatis、配置 Dubbo

---
+++ micr-dataservice/src/main/java/org/example/ylb/dataservice/MicrDataserviceApplication.java
@@ -0,0 +1,19 @@
+package org.example.ylb.dataservice;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 启动 dubbo 服务
+@EnableDubbo
+// Mapper 扫描包
+@MapperScan(basePackages = "org.example.ylb.dataservice.mapper")
+@SpringBootApplication
+public class MicrDataserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrDataserviceApplication.class, args);
+    }
+
+}
