de766e1c3e4abbe3b3d0b4b00687869a154d1a8c
Mrying0823 <1553947217@qq.com>
2023-09-25 00:59:05 +0800

    常量

---
+++ micr-common/src/main/java/org/example/ylb/common/constants/YLBConstant.java
@@ -0,0 +1,18 @@
+package org.example.ylb.common.constants;
+
+/**
+ * @author 邓和颖
+ * @since 2023/9/24 20:40
+ */
+public class YLBConstant {
+
+    // 产品类型
+    // 新手宝
+    public static final int PRODUCT_TYPE_XINSHOUBAO = 0;
+
+    // 优选
+    public static final int PRODUCT_TYPE_YOUXUAN = 1;
+
+    // 散标
+    public static final int PRODUCT_TYPE_SANBIAO = 2;
+}
