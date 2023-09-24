21ec897904a2745afb5f5c3bc68fc760413adfed
Mrying0823 <1553947217@qq.com>
2023-09-25 00:55:35 +0800

    首页产品数据服务注入

--- micr-web/src/main/java/org/example/ylb/front/controller/BaseController.java
+++ micr-web/src/main/java/org/example/ylb/front/controller/BaseController.java
@@ -2,6 +2,7 @@ package org.example.ylb.front.controller;
 
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.example.ylb.api.service.PlatBaseInfoService;
+import org.example.ylb.api.service.ProdInfoService;
 
 /**
  * @author 邓和颖
@@ -10,7 +11,13 @@ import org.example.ylb.api.service.PlatBaseInfoService;
 public class BaseController {
 
     // 声明公共的方法、属性等
+
+    // 平台基本信息服务
     @DubboReference(interfaceClass = PlatBaseInfoService.class,version = "1.0")
     protected PlatBaseInfoService platBaseInfoService;
+
+    // 产品服务
+    @DubboReference(interfaceClass = ProdInfoService.class,version = "1.0")
+    protected ProdInfoService prodInfoService;
 
 }
