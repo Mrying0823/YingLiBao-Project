d7adb3bb63dcd6090397495a44e54b411d24f6eb
Mrying0823 <1553947217@qq.com>
2023-09-27 15:57:47 +0800

    新增手机号敏感处理

--- micr-common/src/main/java/org/example/ylb/common/utils/CommonUtil.java
+++ micr-common/src/main/java/org/example/ylb/common/utils/CommonUtil.java
@@ -1,5 +1,7 @@
 package org.example.ylb.common.utils;
 
+import java.util.regex.Pattern;
+
 /**
  * @author 邓和颖
  * @since 2023/9/24 19:58
@@ -14,5 +16,11 @@ public class CommonUtil {
     // 处理 pageSize
     public static int defaultPageSize(Integer pageSize) {
         return (pageSize == null || pageSize < 1) ? 1 : pageSize;
+    }
+
+    // 手机号敏感处理
+    public static String maskPhone(String phone) {
+        return (phone == null || phone.isEmpty() || !Pattern.matches("^1[3-9]\\d{9}$",phone))
+                ? "***********" : phone.substring(0,3)+"******"+phone.substring(9,11);
     }
 }
