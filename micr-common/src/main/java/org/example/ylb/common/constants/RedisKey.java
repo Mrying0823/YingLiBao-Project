d4c2cd0c7e886a3fbd206e23ec03acf634a806d9
Mrying0823 <1553947217@qq.com>
2023-09-27 15:56:23 +0800

    redis 的键

---
+++ micr-common/src/main/java/org/example/ylb/common/constants/RedisKey.java
@@ -0,0 +1,11 @@
+package org.example.ylb.common.constants;
+
+/**
+ * @author 邓和颖
+ * @since 2023/9/26 16:25
+ */
+public class RedisKey {
+
+    // 投资排行榜
+    public static final String KEY_INVEST_RANK = "INVEST:RANK";
+}
