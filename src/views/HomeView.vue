4ab8ee0cedd9a1eda202fec6085d71d603680c56
Mrying0823 <1553947217@qq.com>
2023-09-30 01:13:35 +0800

    平台基本信息展示

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,306 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!--头部-->
+    <page-header></page-header>
+
+    <!--banner-->
+    <div class="banner-content">
+      <div class="swiper-container banner-lb">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <a href="javascript:" >
+              <img src="@/assets/image/banner.jpg" alt="">
+            </a>
+          </div>
+          <div class="swiper-slide">
+            <a href="javascript:" >
+              <img src="@/assets/image/banner.jpg" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="banner-abs">
+        <div class="banner-abs-box">
+          <div class="banner-abs-title">动力金融网历史年化收益率</div>
+          <b>{{platInfo.historyAvgRate}}<i>%</i></b>
+          <p>平台用户数</p>
+          <span>{{platInfo.registerUsers}}<i>位</i></span>
+          <p class="banner-abs-border">累计成交金额</p>
+          <span>{{platInfo.sumBidMoney}}<i>元</i></span>
+        </div>
+      </div>
+    </div>
+    <div class="banner-list">
+      <ul>
+        <li>
+          <img src="@/assets/image/banner-tag1.png" alt="">
+          <p>
+            <b>实力雄厚</b>
+            <span>亿级注册资本 ,千万技术投入</span>
+          </p>
+        </li>
+        <li>
+          <img src="@/assets/image/banner-tag2.png" alt="">
+          <p>
+            <b>风控严苛</b>
+            <span>30道风控工序，60项信用审核</span>
+          </p>
+        </li>
+        <li>
+          <img src="@/assets/image/banner-tag3.png" alt="">
+          <p>
+            <b>投资省心</b>
+            <span>资金安全风控，银行安全托管</span>
+          </p>
+        </li>
+      </ul>
+    </div>
+
+    <!--产品-->
+    <div class="content">
+      <h2 class="public-title"><span>新手宝</span></h2>
+      <div class="new-user">
+        <div class="new-user-sm">
+          <span>1000.0元起投</span>
+          <span>投资最高限额10000.0元</span>
+          <span>当日即系</span>
+        </div>
+        <div class="new-user-number">
+          <ul>
+            <li>
+              <p><b>4.9</b>%</p>
+              <span>历史年化收益率</span>
+            </li>
+            <li>
+              <p><b>1</b>个月</p>
+              <span>投资周期</span>
+            </li>
+            <li>
+              <p><b>250000</b>元</p>
+              <span>余利可投资金额</span>
+            </li>
+          </ul>
+          <a href="details.html" target="_blank" class="new-user-btn">立即投资</a>
+        </div>
+        <span class="new-tag">新用户专享</span>
+      </div>
+
+      <h2 class="public-title"><span>优选产品</span> <a href="list.html" target="_blank" class="public-title-more">查看更多产品>></a></h2>
+      <ul class="preferred-select clearfix">
+        <li>
+          <h3 class="preferred-select-title">
+            <span>满月宝</span>
+            <img src="@/assets/image/1-bg1.jpg" alt="">
+          </h3>
+          <div class="preferred-select-number">
+            <p><b>4.9</b>%</p>
+            <span>历史年化收益率</span>
+          </div>
+          <div class="preferred-select-date">
+            <div>
+              <span>投资周期</span>
+              <p><b>1</b>个月</p>
+            </div>
+            <div>
+              <span>余利可投资金额</span>
+              <p><b>250000.0</b>元</p>
+            </div>
+          </div>
+          <p class="preferred-select-txt">
+            优选计划项目，投资回报周期1个月，起点低，适合短期资金周转、对流动性要求高的投资人。
+          </p>
+          <a href="javascript:" target="_blank" class="preferred-select-btn">立即投资</a>
+        </li>
+        <li>
+          <h3 class="preferred-select-title">
+            <span>季度宝</span>
+            <img src="@/assets/image/1-bg2.jpg" alt="">
+          </h3>
+          <div class="preferred-select-number">
+            <p><b>6.2</b>%</p>
+            <span>历史年化收益率</span>
+          </div>
+          <div class="preferred-select-date">
+            <div>
+              <span>投资周期</span>
+              <p><b>1</b>个月</p>
+            </div>
+            <div>
+              <span>余利可投资金额</span>
+              <p class="preferred-select-date-no"><b>0.0</b>元</p>
+            </div>
+          </div>
+          <p class="preferred-select-txt">
+            优选计划项目，投资回报周期1个月，起点低，适合短期资金周转、对流动性要求高的投资人。
+          </p>
+          <a href="javascript:" target="_blank" class="preferred-select-btn">立即投资</a>
+        </li>
+        <li>
+          <h3 class="preferred-select-title">
+            <span>双季宝</span>
+            <img src="@/assets/image/1-bg3.jpg" alt="">
+          </h3>
+          <div class="preferred-select-number">
+            <p><b>7.1</b>%</p>
+            <span>历史年化收益率</span>
+          </div>
+          <div class="preferred-select-date">
+            <div>
+              <span>投资周期</span>
+              <p><b>1</b>个月</p>
+            </div>
+            <div>
+              <span>余利可投资金额</span>
+              <p><b>250000.0</b>元</p>
+            </div>
+          </div>
+          <p class="preferred-select-txt">
+            优选计划项目，投资回报周期1个月，起点低，适合短期资金周转、对流动性要求高的投资人。
+          </p>
+          <a href="javascript:" target="_blank" class="preferred-select-btn">立即投资</a>
+        </li>
+      </ul>
+
+      <h2 class="public-title"><span>散标产品</span> <a href="list.html" target="_blank" class="public-title-more">查看更多产品>></a></h2>
+      <ul class="preferred-select clearfix">
+        <li>
+          <h3 class="preferred-select-title1">个人信用消费借款
+            <span>散标</span>
+          </h3>
+          <div class="preferred-select-number">
+            <p><b>4.9</b>%</p>
+            <span>历史年化收益率</span>
+          </div>
+          <div class="preferred-select-date">
+            <div>
+              <span>投资周期</span>
+              <p><b>1</b>个月</p>
+            </div>
+            <div>
+              <span>余利可投资金额</span>
+              <p><b>250000.0</b>元</p>
+            </div>
+          </div>
+          <p class="preferred-select-txt">
+            优选计划项目，投资回报周期1个月，起点低，适合短期资金周转、对流动性要求高的投资人。
+          </p>
+          <a href="javascript:" target="_blank" class="preferred-select-btn">立即投资</a>
+        </li>
+        <li>
+          <h3 class="preferred-select-title1">个人信用消费借款
+            <span>散标</span>
+          </h3>
+          <div class="preferred-select-number">
+            <p><b>6.2</b>%</p>
+            <span>历史年化收益率</span>
+          </div>
+          <div class="preferred-select-date">
+            <div>
+              <span>投资周期</span>
+              <p><b>1</b>个月</p>
+            </div>
+            <div>
+              <span>余利可投资金额</span>
+              <p class="preferred-select-date-no"><b>0.0</b>元</p>
+            </div>
+          </div>
+          <p class="preferred-select-txt">
+            优选计划项目，投资回报周期1个月，起点低，适合短期资金周转、对流动性要求高的投资人。
+          </p>
+          <a href="javascript:" target="_blank" class="preferred-select-btn">立即投资</a>
+        </li>
+        <li>
+          <h3 class="preferred-select-title1">个人信用消费借款
+            <span>散标</span>
+          </h3>
+          <div class="preferred-select-number">
+            <p><b>7.1</b>%</p>
+            <span>历史年化收益率</span>
+          </div>
+          <div class="preferred-select-date">
+            <div>
+              <span>投资周期</span>
+              <p><b>1</b>个月</p>
+            </div>
+            <div>
+              <span>余利可投资金额</span>
+              <p><b>250000.0</b>元</p>
+            </div>
+          </div>
+          <p class="preferred-select-txt">
+            优选计划项目，投资回报周期1个月，起点低，适合短期资金周转、对流动性要求高的投资人。
+          </p>
+          <a href="javascript:" target="_blank" class="preferred-select-btn">立即投资</a>
+        </li>
+      </ul>
+
+    </div>
+
+    <!--说明-->
+    <div class="information-box">
+      <ul>
+        <li>
+          <img src="@/assets/image/2-icon1.png" alt="">
+          <p>优质借款</p>
+          <span>严苛风控，多重审核</span>
+
+        </li>
+        <li>
+          <img src="@/assets/image/2-icon2.png" alt="">
+          <p>次日计息</p>
+          <span>闪电成交，谁比我快</span>
+        </li>
+        <li>
+          <img src="@/assets/image/2-icon3.png" alt="">
+          <p>全年无休</p>
+          <span>百万用户，一路同行</span>
+        </li>
+        <li>
+          <img src="@/assets/image/2-icon4.png" alt="">
+          <p>知心托付</p>
+          <span>百万用户，一路同行</span>
+        </li>
+        <li>
+          <img src="@/assets/image/2-icon5.png" alt="">
+          <p>技术保障</p>
+          <span>千万投入，专注研发</span>
+        </li>
+      </ul>
+    </div>
+
+    <!--公共底部-->
+    <page-footer></page-footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import PageHeader from "@/components/PageHeader";
+import PageFooter from "@/components/PageFooter";
+import axios from "axios";
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    PageHeader,
+    PageFooter,
+  },
+  data() {
+    // 返回一个包含数据属性的对象，这些属性将在组件的实例化过程中添加到组件实例中
+    return {
+      platInfo: {historyAvgRate: 0.00,sumBidMoney: 0.00,registerUsers: 0},
+    }
+  },
+  mounted() {
+    // 页面加载至浏览器，执行 mounted
+    // 向服务器发起请求，获取数据，更新页面
+    axios.get("http://localhost:8000/api/v1/plat/info").then(response => {
+
+      // 如果 response 不为空
+      if(response) {
+        this.platInfo = response.data.retData;
+      }
+    });
   }
 }
 </script>
