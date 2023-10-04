c947cced6df2c480ec48be4824c864b95994e6e8
Mrying0823 <1553947217@qq.com>
2023-10-05 01:08:25 +0800

    增加手机号格式检验

--- micr-common/src/main/java/org/example/ylb/common/utils/CommonUtil.java
+++ micr-common/src/main/java/org/example/ylb/common/utils/CommonUtil.java
@@ -23,4 +23,9 @@ public class CommonUtil {
         return (phone == null || phone.isEmpty() || !Pattern.matches("^1[3-9]\\d{9}$",phone))
                 ? "***********" : phone.substring(0,3)+"******"+phone.substring(9,11);
     }
+
+    // 手机号格式检验
+    public static boolean checkPhone(String phone) {
+        return Pattern.matches("^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\\d{8}$", phone);
+    }
 }
