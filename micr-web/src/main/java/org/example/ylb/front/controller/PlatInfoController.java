a480dfcfb528a1f95b5c29e72a61fa1d4dbcebab
Mrying0823 <1553947217@qq.com>
2023-10-04 10:36:15 +0800

    更新

--- micr-web/src/main/java/org/example/ylb/front/controller/PlatInfoController.java
+++ micr-web/src/main/java/org/example/ylb/front/controller/PlatInfoController.java
@@ -1,9 +1,9 @@
 package org.example.ylb.front.controller;
 
-import io.swagger.annotations.ApiOperation;
-import org.example.ylb.front.view.RespResult;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.example.ylb.api.pojo.plat.BaseInfo;
+import org.example.ylb.front.view.RespResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
