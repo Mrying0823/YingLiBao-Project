0c6d780926a6b70ec395e2ab4583a20028a719f7
Mrying0823 <1553947217@qq.com>
2023-10-06 16:34:59 +0800

    开通资金账户

--- micr-dataservice/src/main/java/org/example/ylb/dataservice/mapper/FinanceAccountMapper.java
+++ micr-dataservice/src/main/java/org/example/ylb/dataservice/mapper/FinanceAccountMapper.java
@@ -19,4 +19,6 @@ public interface FinanceAccountMapper {
     int updateByPrimaryKeySelective(FinanceAccount record);
 
     int updateByPrimaryKey(FinanceAccount record);
+
+    int insertNewFinanceAccount(FinanceAccount financeAccount);
 }
