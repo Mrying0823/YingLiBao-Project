1904ffa6a60fbaef33ed853a487e614bbed2d271
Mrying0823 <1553947217@qq.com>
2023-10-08 21:29:16 +0800

    使用 qs.js 将 json 对象转为查询字符串，用于 post

--- src/axios/httpRequest.js
+++ src/axios/httpRequest.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import qs from "qs";
 
 // 设置默认值
 axios.defaults.baseURL = "http://localhost:8000/api";
@@ -13,13 +14,24 @@ function toGet(url,params) {
     });
 }
 
-// post 请求方法，url 是请求的地址，params 是请求的 json 对象
-function doPost(url,params) {
+// post 请求方法，url 是请求的地址，data 是请求的 json 对象
+// eslint-disable-next-line no-unused-vars
+function doPostJson(url,data) {
     return axios({
         url: url,
         method: "post",
-        params: params
+        data: data
     });
+}
+
+// post 请求方法，将 json 对象数据转为 qs 格式
+function doPost(url,data) {
+    let requestData = qs.stringify(data);
+    return axios({
+        url: url,
+        method: "post",
+        data: requestData
+    })
 }
 
 // 导出函数
