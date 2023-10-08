141348a35e49eb89750ebc13a6419b0a87a4df88
Mrying0823 <1553947217@qq.com>
2023-10-08 21:27:33 +0800

    npm 安装 js-md5、qs、crypto-browserify，解决 Module not found: Error: Can't resolve 'crypto' in '\node_modules\js-md5\src' 问题

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+// 头部引入
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: { // 环境配置
     host: '0.0.0.0',
     port: '8080',
     https: false,
+  },
+
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
   }
 })
