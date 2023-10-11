edf68c455de77d70b1230c09ff6251e7604e0dbd
Mrying0823 <1553947217@qq.com>
2023-10-12 01:23:52 +0800

    web 服务启动类生成 JWT 对象

--- micr-web/src/main/java/org/example/ylb/MicrWebApplication.java
+++ micr-web/src/main/java/org/example/ylb/MicrWebApplication.java
@@ -2,8 +2,11 @@ package org.example.ylb;
 
 import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.example.ylb.common.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -14,6 +17,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwaggerBootstrapUI
 @SpringBootApplication
 public class MicrWebApplication {
+
+	@Value("${jwt.data.secret}")
+	private String secret;
+
+	@Value("${jwt.data.expiration}")
+	private long expiration;
+
+	// 创建 JwtUtils 对象
+	@Bean
+	public JwtUtils jwtUtils() {
+		return new JwtUtils(secret,expiration);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicrWebApplication.class, args);
