21f9a772d6100f4c3200d9c6ddedb2b10d1c6d5e
Mrying0823 <1553947217@qq.com>
2023-10-13 01:27:35 +0800

    删除一些基于 Spring Security UserDetail 的方法

--- micr-common/src/main/java/org/example/ylb/common/utils/JwtUtils.java
+++ micr-common/src/main/java/org/example/ylb/common/utils/JwtUtils.java
@@ -3,13 +3,10 @@ package org.example.ylb.common.utils;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
-import org.example.ylb.common.constants.JwtKey;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -31,13 +28,26 @@ public class JwtUtils {
         this.expiration = expiration;
     }
 
-    // 根据用户名生成 token
-    // 传入的是使用 SpringSecurity 里的 UserDetails
-    public String createToken(UserDetails userDetails) {
-        HashMap<String, Object> claims = new HashMap<>();
-        claims.put(JwtKey.CLAIM_KEY_PHONE, userDetails.getUsername());
-        claims.put(JwtKey.CLAIM_KEY_DATE, userDetails.getUsername());
-        return createToken(claims);
+    // 根据负载生成 jwt token
+    public String createToken(Map<String, Object> claims) {
+
+        // 用于在 JWT（JSON Web Token）的签名和验证过程中使用 HMAC-SHA 签名算法
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        // jjwt 构建 jwt builder
+        // setId 设置JWT令牌的唯一标识符
+        // setClaims 将准备好的声明信息全部设置到 jwt 中，再次设置即覆盖
+        // setIssuedAt 设置令牌发放时间
+        // setExpiration 设置令牌过期时间
+        // signWith 设置签名
+        // compact 将 JWT 令牌转换为字符串表示
+        return Jwts.builder()
+                .setId(UUIDUtils.getUUID())
+                .setClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(expirationDate())
+                .signWith(SignatureAlgorithm.HS256,secretKey)
+                .compact();
     }
 
     // 根据 token 获取用户名
@@ -68,29 +78,6 @@ public class JwtUtils {
         return claims;
     }
 
-
-    // 根据负载生成 jwt token
-    private String createToken(Map<String, Object> claims) {
-
-        // 用于在 JWT（JSON Web Token）的签名和验证过程中使用 HMAC-SHA 签名算法
-        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-
-        // jjwt 构建 jwt builder
-        // setId 设置JWT令牌的唯一标识符
-        // setClaims 将准备好的声明信息全部设置到 jwt 中，再次设置即覆盖
-        // setIssuedAt 设置令牌发放时间
-        // setExpiration 设置令牌过期时间
-        // signWith 设置签名
-        // compact 将 JWT 令牌转换为字符串表示
-        return Jwts.builder()
-                .setId(UUIDUtils.getUUID())
-                .setClaims(claims)
-                .setIssuedAt(new Date())
-                .setExpiration(expirationDate())
-                .signWith(SignatureAlgorithm.HS256,secretKey)
-                .compact();
-    }
-
     // 生成 token 失效时间
     private Date expirationDate() {
         // 失效时间为：系统当前毫秒数+我们设置的时间（s）*1000=>毫秒
@@ -99,12 +86,12 @@ public class JwtUtils {
     }
 
     // 判断 token 是否有效
-    public boolean validateToken(String token, UserDetails userDetails) {
+    public boolean validateToken(String token, Map<String, Object> claims) {
         // 判断 token 是否过期
         // 判断 token 是否和 userDetails 中的一致
         // 我们要做的是先获取用户名
         String username = getUsernameFromToken(token);
-        return username.equals(userDetails.getUsername()) && isTokenExpired(token);
+        return username.equals(claims.get("name")) && isTokenExpired(token);
     }
 
     // 判断 token、是否失效
@@ -119,19 +106,4 @@ public class JwtUtils {
         Claims claims = getClaimsFromToken(token);
         return claims.getExpiration();
     }
-
-    // 判断 token 是否可以被刷新
-    // 过期（销毁）就可以
-    public boolean canBeRefreshed(String token){
-        return isTokenExpired(token);
-    }
-
-    // 刷新 token
-    public String refreshToken(String token){
-        Claims claims = getClaimsFromToken(token);
-        // 修改为当前时间
-        claims.put(JwtKey.CLAIM_KEY_DATE,new Date());
-        return createToken(claims);
-    }
-
 }
